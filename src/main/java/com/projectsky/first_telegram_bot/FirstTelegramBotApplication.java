b2aa8e08c083784ee3f39e19882fd5fd29d06c3c
projectsky5 <mackvej@gmail.com>
2025-05-30 11:38:50 +0300

    Initial commit

---
+++ src/main/java/com/projectsky/first_telegram_bot/FirstTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.projectsky.first_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTelegramBotApplication.class, args);
+    }
+
+}
